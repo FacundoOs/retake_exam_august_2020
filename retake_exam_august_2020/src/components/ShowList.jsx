280547c62f6d189282a13311977c6aa1c7de4e95
FacundoOs <osores.facundo@gmail.com>
2020-10-14 11:11:23 +0200

    Install axios. Add get request and map function

--- retake_exam_august_2020/src/components/ShowList.jsx
+++ retake_exam_august_2020/src/components/ShowList.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ShowList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default ShowList
+  useEffect(() => {
+    const fetchData = async () => {
+      let result = await axios.get(
+        "https://content.viaplay.se/pc-se/serier/samtliga"
+      );
+      setData(
+        result.data._embedded["viaplay:blocks"][0]._embedded["viaplay:products"]
+      );
+    };
+    fetchData();
+  }, []);
+
+  let dataShow = data.map((show) => {
+    return (
+      <div>
+        <img src={`${show.content.images.lansscape.url}`}></img>
+      </div>
+    );
+  });
+
+  return <div></div>;
+};
+
+export default ShowList;
